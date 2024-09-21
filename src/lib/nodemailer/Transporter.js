2cd39d711a0808d59e34bee673393b8097d103fd
Anthony Reynolds <RBMarketingAndAnalytics@gmail.com>
2024-09-21 13:22:55 -0400

    send email in production fix nodemailer

--- src/lib/nodemailer/Transporter.js
+++ src/lib/nodemailer/Transporter.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer'
 export const transporter = nodemailer.createTransport({
   service: process.env.EMAIL_SERVER_SERVICE,
   host: process.env.EMAIL_SERVER_HOST,
-  port: process.env.EMAIL_SERVER_PORT,
-  secure: true,
+  port: '587',
+  secure: false,
   auth: {
     user: process.env.EMAIL_SERVER_USER,
     pass: process.env.EMAIL_SERVER_PASSWORD,
